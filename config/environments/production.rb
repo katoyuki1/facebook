0faf6e694b0fa7979af91970e1580943ee71ea16
katoyuki <user@USER-no-MacBook-Pro.local>
2016-10-22 21:45:44 +0900

    メール　ドメイン設定２

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   {
    user_name: ENV['SENDGRID_USERNAME'],
    password: ENV['SENDGRID_PASSWORD'],
-   domain: "heroku.com",
+   domain: "52.198.101.212/",
    address: "smtp.sendgrid.net",
    port: 2525,
    authentication: :plain,
